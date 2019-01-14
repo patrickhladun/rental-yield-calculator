5dfdf8e0fbb94a6ce8e840ce8592ce6b3e53d412
Patrick Hladun <patrickhladun@gmail.com>
2019-01-14 15:55:27 +0000

    First draft of the calculator.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Sandbox Starter changelog
 
+## Release Version 1.1.0 - Monday 14 January 2019
+
+- First draft of the calculator.
+
 ## Release Version 1.0.0 - Friday 04 January 2019
 
 - Initial version of the Rental Yied Calculator
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,28 +1,74 @@
-var rentalIncome;
-var propertyValue;
+var rentalIncome = '';
+var propertyValue = '';
 var form = document.getElementById('ryc__form');
 var results = document.getElementById('ryc__result');
+var inputs = document.querySelectorAll('.currency');
 
-form.addEventListener("keypress", function(e) {
-    if (e.keyCode === 46) {
-        e.preventDefault();
+for(var i = 0; i < inputs.length; i++) {
+
+    inputs[i].addEventListener("keyup", function() {
+        formatCurrency(this);
+    });
+
+}
+
+function formatNumber(n) {
+    return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+function formatCurrency(input) {
+    var inputValue = input.value;
+    var inputLenght = inputValue.length;
+    var iCaretPos = input.selectionStart;
+
+    if(inputValue === '') { return };
+
+    if(inputValue.indexOf('.') >= 0) {
+        var decimalPosition = inputValue.indexOf('.');
+
+        var leftSide = inputValue.substring(0, decimalPosition);
+        var rightSide = inputValue.substring(decimalPosition);
+
+        rightSide = rightSide.substring(0,3);
+
+        leftSide = formatNumber(leftSide);
+        rightSide = formatNumber(rightSide);
+
+        inputValue = '£' + leftSide + '.' + rightSide;
+    } else {
+        inputValue = formatNumber(inputValue);
+        inputValue = '£' + inputValue;
     }
-});
+
+    input.value = inputValue;
+
+    var inputUpdatedLength = inputValue.length;
+    iCaretPos = inputUpdatedLength - inputLenght + iCaretPos;
+    input.setSelectionRange(iCaretPos, iCaretPos);
+}
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    rentalIncome = form['ryc__rental-income'].value;
-    propertyValue = form['ryc__property-value'].value;
+
+    rentalIncome = form['ryc__rental-income'].value.replace(/[£,]/g, '');
+    propertyValue = form['ryc__property-value'].value.replace(/[£,]/g, '');
+
+    console.log(rentalIncome);
+    console.log(propertyValue);
 
     if(rentalIncome <= 0 || propertyValue <= 0 ) {
         results.innerHTML = '0';
     } else {
         var calc;
-        calc = (rentalIncome / propertyValue) * 100;
+        calc = (rentalIncome * 52) / propertyValue * 100;
+        console.log(calc);
         calc = calc.toFixed(1);
-        results.innerHTML = calc + '%';
+        calc = calc.split('.');
+        results.innerHTML = '<p>Rental Yield:</p><p><span class="ryc__result-num">'
+            + calc[0]
+            + '</span><span class="ryc__result-decimal">. '
+            + calc[1]
+            + '%</span></p>';
     }
-
 });
-
 
